6d1426f72336899ed902d4357f65f197190d8490
clock783 <aodunlade3@gmail.com>
2023-08-13 17:01:06 -0400

    Setup Files and create base format
    - Insert div with js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+const mainBoard = document.querySelector("#mainBoard");
+
+let div = document.createElement("div");
+div.style.width = "300px";
+div.style.height = "300px";
+div.style.background = "blue";
+
+document.getElementById("mainBoard").appendChild(div);
+
